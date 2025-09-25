36a02fca393658a3ff3211623e71e6a6be125cf0
Alejandro <allaro264@gmail.com>
2025-09-25 13:46:00 +0200

    fix: "WIP"
    arreglra auth.ts

--- models/character.ts
+++ models/character.ts
@@ -1,53 +1,50 @@
-import { minLength, object, pipe, string, type InferInput } from "valibot";
+import { minLength, string, object, pipe, type InferInput } from "valibot";
 
-export const CharacterSchema = object({
+export const CharacterSchema = object ({
     name: pipe(string(), minLength(6)),
-    lastNmae: pipe(string(), minLength(6)),
+    lastName: pipe(string(), minLength(6))
 })
 
-export type Character = InferInput<typeof CharacterSchema> & {id: number};
+export type Character = InferInput<typeof CharacterSchema> & { id: number};
 
-const Characters: Map<number, Character> = new Map();
+const characters: Map<number, Character> = new Map();
 
-export const getAllCharacters = ()  : Character[] =>  {
-    return Array.from(Characters.values())
+export const getAllCharacters = (): Character[] => {
+    return Array.from(characters.values())
 }
 
-export const getCharacterById = (id: number) : Character | undefined => {
-    return Characters.get(id);
+export const getChracterById = (id: number): Character | undefined => {
+    return characters.get(id);
 }
 
-export const addCharacter = (character: Character) : Character => {
-    if(!Characters.has(character.id)) {
-        console.error("Character with id ", character.id, " already exists")
-        return character;
-    }
+export const addCharacter = (character: Character): Character => {
 const newCharacter = {
     ...character,
     id: new Date().getTime()
 }
 
-Characters.set(newCharacter.id, newCharacter);
+characters.set(newCharacter.id, newCharacter);
+
 return newCharacter;
 }
 
-export const updateCharacter = (id: number, upadateCharacter: Character) : Character | null => {
-    if (!Characters.has(id)) {
-        console.error("character not found")
+export const updateCharacter = (id: number, updateCharacter: Character): Character | null => {
+    if (!characters.has(id)) {
+        console.error('Character with id', id, 'not found');
         return null;
     }
 
-    Characters.set(id, upadateCharacter);
-    // si no existe lo creo y si existe lo remplazo
-    return upadateCharacter;
+    characters.set(id, updateCharacter);
+    //if it doesn't exist, I create it, and if it does exist, I replace it
+    return updateCharacter;
 }
 
-export const deleteCharacter = (id: number) : boolean => {
-   if (!Characters.has(id)) {
-    console.error("charactet not found");
-    return false;
-   }
+export const deleteCharacter = (id: number): boolean => {
+    if (!characters.has(id)) {
+        console.error('Character with id ', id, ' not found');
+        return false;
+    }
 
-   Characters.delete(id);
-   return true;
+    characters.delete(id);
+    return true;
 }
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,101 +1,62 @@
-import { addRevokeToken, authSchema, HttpMethod } from "../models";
 import type { IncomingMessage, ServerResponse } from "http";
-import {  safeParse } from "valibot";
-import config from "../config";
+import { authSchema, createUser, findUserByEmail, HttpMethod, validatePassword } from "../models";
+import { safeParse } from "valibot";
 import { parseBody } from "../utils/parseBody";
-import { createUser, findUserByEmail, revokeUserToken, validatePassword } from "../models/user"; 
 import { sign } from "jsonwebtoken";
-import type { AuthenticatedRequest } from "../middleware/authentication";
+import { config } from "process";
+
 
 export const authRouter = async (req: IncomingMessage, res: ServerResponse) => {
-    const { method, url} = req;
+    const { method, url } = req;
 
     if (url === "/auth/register" && method === HttpMethod.POST) {
-        const body = await parseBody(req);
-        const result = await safeParse(authSchema, body);
-        if (result.issues) {
-            res.statusCode = 400;
-            res.end(JSON.stringify({ message: "Bad Request"}));
-            return;
-        }
-
-        const { email, password} = body;
-
-        try {
-            const user = await createUser(email, password);
-
-            res.statusCode = 201;
-            res.end(JSON.stringify({ id: user.id, email: user.email}));
-            return;
-        }catch (err) {
-            if (err instanceof Error) {
-                res.end(JSON.stringify({ message: err.message}));
-            }else {
-                res.end(JSON.stringify({ message: "Internal Server Error"}));
-            }
-            return;
-        }
-    }
-    if (url === "/auth/login" && method === HttpMethod.POST) {
         const body = await parseBody(req);
         const result = safeParse(authSchema, body);
 
         if (result.issues){
+            res.statusCode= 400;
+            res.end(JSON.stringify({message: "Bad Request"}));
+        }
+
+        const {email, password} = body;
+
+        try{
+            const user = await createUser(email, password);
+
+            res.statusCode = 201;
+            res.end(JSON.stringify(user));
+        } catch (err) {
+            if (err instanceof Error){
+                res.end(JSON.stringify({ message: err.message}));
+            }else{
+                res.end(JSON.stringify({message: "Intenal server Error"}))
+            }
+        }
+    }
+
+    if (url === "/auth/login" && method === HttpMethod.POST){
+        const body = await parseBody(req);
+        const result = safeParse(authSchema, body);
+
+        if (result.issues) {
             res.statusCode = 400;
             res.end(JSON.stringify({ message: "Bad Request"}));
         }
 
-        const { email,password} = body;
+        const {email, password} = body;
         const user = findUserByEmail(email);
 
-        if (!user || !(await validatePassword(user, password))){
+        if (!user || !(await validatePassword(user.password))){
             res.statusCode = 401;
-            res.end(JSON.stringify({ message: "Invalid email or password"}))
+            res.end(JSON.stringify({ message: "Invalid email or password"}));
             return;
         }
 
-        const accessToken = sign(
-            {id: user.id, email: user.email, role: user.role}, 
-            config.jwtSecret, 
-            {expiresIn: "1h"}
+        const refresToken = sign(
+            {id: user.id},
+            config.jwtSecret,
+            {expresIn: "1h"}
         )
-
-         const refreshToken = sign(
-            {id: user.id,}, 
-            config.jwtSecret, 
-            {expiresIn: "1d"}
-        )
-
-        user.refreshToken = refreshToken;
-
-        res.end(JSON.stringify({ accessToken, refreshToken}));
-        return;
+        
     }
-
-    if(url === "/auth/refresh" && method === HttpMethod.POST) {
-        const token = req.headers["authorization"]?.split(" ")[1];
-
-        if (token) {
-        addRevokeToken(token);
-
-        const formattReq = req as AuthenticatedRequest;
-            if (
-                formattReq.user &&
-                typeof formattReq.user === "object" &&
-                "id" in formattReq.user
-            ) {
-               const result = revokeUserToken(formattReq.user.email);
-               if (!result) {
-                res.statusCode = 403;
-                res.end(JSON.stringify({ message: "Forbidden"}));
-               }
-            }
-
-            res.end(JSON.stringify({ message: "Loggeed out"}));
-            return;
-        }
-    }
-
-    res.statusCode = 404;
-    res.end(JSON.stringify({ message: "Not Found"}));
 }
